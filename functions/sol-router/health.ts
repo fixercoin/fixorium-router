1a90196245ce9298300c60744dccc86e14cb243d
fixorium <khanbabusargodhaa@gmail.com>
2026-04-27 06:01:53 +0500

    Update health.ts

--- functions/sol-router/health.ts
+++ functions/sol-router/health.ts
@@ -7,6 +7,7 @@ export const onRequest: PagesFunction = async () => {
     return new Response(JSON.stringify({
         status: 'healthy',
         router: 'MAX ROUTER',
+        domain: 'https://fixorium.com.pk',
         version: '2.0.0',
         fee: '0.01%',
         dexes: 30,
